6b99c4066ee2dfd3eeed425990d49fd69ffedd98
Maxi <maxi_ruiz140@hotmail.com>
2017-12-15 18:25:43 -0300

    firebase con routing malo

--- src/component/Info.js
+++ src/component/Info.js
@@ -3,11 +3,32 @@ import React, { Component } from 'react';
 // Assets
 
 class Info extends Component {
+    constructor() {
+        super();
+        this.state = {
+            count: 0
+        };
+        this.handleCountClick = this.handleCountClick.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
+    handleCountClick(e){
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
 
     render() {
+        console.log('render');
         return (
             <div className="Info">
-                <h1>Info</h1>
+                <h1>{this.state.count}</h1>
+                <button id="result" onClick={this.handleCountClick}>+</button>
             </div>
         );
     }
--- src/component/Stock.js
+++ src/component/Stock.js
@@ -6,35 +6,65 @@ import * as firebase from 'firebase';
 class Stock extends Component {
 
     constructor(){
+        console.log('constructor');
         super();
         this.state = {
-            listOfStock: []
+            listOfStock: [],
+            list: [],
+            rootRef : firebase.database().ref().child('stock')
         };
+
+        this.setList = this.setList.bind(this);
+        this.clearList = this.clearList.bind(this);
     }
 
-    componentDidMount(){
-        const rootRef = firebase.database().ref().child('stock');
-        rootRef.on('child_added', snap => {
-            console.log(snap.key);
-            console.log(snap.val().name);
-            console.log(snap.val().price);
+    componentWillMount(){
+        console.log('will mount');
+        //this.clearList();
+        //let rootRef = firebase.database().ref().child('stock');
+        this.state.rootRef.on('value', snapshot => {
+            snapshot.forEach(childSnapshot => {
+                const stock = {
+                    id: childSnapshot.key,
+                    name: childSnapshot.val().name,
+                    price: childSnapshot.val().price
+                };
+                this.setList(stock);
+            });
+        });
+    }
 
+    setList(stock){
+        if(this.state.listOfStock.length === 0) {
+            this.setState({
+                listOfStock:[]
+            });
+        }
+        console.log(stock);
+        this.setState({
+            listOfStock: this.state.listOfStock.concat([stock])
+        });
+    }
+
+    clearList(){
+        console.log('clear');
+        this.setState({
+            listOfStock: [],
+            rootRef : firebase.database().ref().child('stock')
         });
     }
 
 
     render() {
+        console.log(this.state.listOfStock);
         const listOfStock = this.state.listOfStock.map(stock =>
                 <div>
-                    <h1>{stock.id}</h1>
-                    <h1>{stock.name}</h1>
-                    <h1>{stock.price}</h1>
+                    <h5>{stock.name} ----> {stock.price}</h5>
                 </div>
         );
         return (
             <div>{listOfStock}</div>
         );
-
     }
 }
 
